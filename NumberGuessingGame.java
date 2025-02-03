b441007d80b69799c3bad26386b48a22ac2fe4a5
Darshan Lohakare <119281681+Darshanlohakare@users.noreply.github.com>
2025-02-03 13:39:22 +0530

    Update NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -6,13 +6,13 @@ public class NumberGuessingGame {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
         boolean playAgain = true;
-        int score = 0; // Tracks rounds won
+        int score = 0; 
 
         System.out.println("Welcome to the Number Guessing Game!");
 
         while (playAgain) {
-            int targetNumber = random.nextInt(100) + 1; // Generate a random number (1 to 100)
-            int attempts = 5; // Set max attempts
+            int targetNumber = random.nextInt(100) + 1; 
+            int attempts = 5; 
             boolean guessedCorrectly = false;
 
             System.out.println("\nI have picked a number between 1 and 100.");
@@ -25,7 +25,7 @@ public class NumberGuessingGame {
                 if (userGuess == targetNumber) {
                     System.out.println("Congratulations! You guessed the correct number.");
                     guessedCorrectly = true;
-                    score++; // Increase score for a win
+                    score++; 
                     break;
                 } else if (userGuess > targetNumber) {
                     System.out.println("Too high! Try again.");
@@ -38,13 +38,13 @@ public class NumberGuessingGame {
                 System.out.println("Sorry, you're out of attempts. The correct number was: " + targetNumber);
             }
 
-            // Ask if user wants to play again
+            
             System.out.print("Do you want to play another round? (y/n): ");
             String response = sc.next();
             playAgain = response.equalsIgnoreCase("y");
         }
 
-        // Display final score
+        
         System.out.println("\nGame Over! You won " + score + " round(s).");
         System.out.println("Thanks for playing!");
 
